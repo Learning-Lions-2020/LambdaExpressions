3ac7cef1f493cc60ae37acab39156d087659d824
tmoechel-futura <t.moechel@futura-solutions.de>
2024-11-30 10:17:55 +0100

    add anonymous and lambda expresion

--- LambdaExpressions/Program.cs
+++ LambdaExpressions/Program.cs
@@ -1,17 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LambdaExpressions;
 
-double Add(double a, double b)
-{
-    return a + b;
-}
-
 double Multiply(double a, double b)
 {
     return a * b;
 }
 
+CalculateAction MultiplyDelegate = delegate (double a, double b)
+{
+    return a * b;
+};
 
-var result = DelegateCalculator.Calculate(Multiply, 4, 5);
 
-Console.WriteLine($"Calculation result: {result.ToString()}");
+var result1 = DelegateCalculator.Calculate(
+    delegate (double a, double b) { return a + b; }, 
+    4, 5);
+
+// change to lambda expression
+var result2 = DelegateCalculator.Calculate(
+    (double a, double b) => a + b,
+    4, 5);
+
+
+var result3 = DelegateCalculator.Calculate(
+    MultiplyDelegate,4,5);
+
+Console.WriteLine($"Calculation result: {result1.ToString()}");
+Console.WriteLine($"Calculation result: {result2.ToString()}");
