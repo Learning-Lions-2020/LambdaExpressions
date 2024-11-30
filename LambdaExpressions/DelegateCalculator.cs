282de7d0502e794dcf4648a1035dd488822405b7
tmoechel-futura <t.moechel@futura-solutions.de>
2024-11-30 09:48:39 +0100

    Add project files.

---
+++ LambdaExpressions/DelegateCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System.Transactions;
+
+namespace LambdaExpressions;
+
+// defines a delegate type that can point to a function that receives 2 double values
+// and returns a double value
+public delegate double CalculateAction(double firstValue, double secondValue);
+
+internal static class DelegateCalculator
+{
+    public static double Calculate(CalculateAction action, double firstValue, double secondValue)
+    {
+        return action.Invoke(firstValue, secondValue);
+    }
+}
---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LambdaExpressions;
+
+double Add(double a, double b)
+{
+    return a + b;
+}
+
+double Multiply(double a, double b)
+{
+    return a * b;
+}
+
+
+var result = DelegateCalculator.Calculate(Multiply, 4, 5);
+
+Console.WriteLine($"Calculation result: {result.ToString()}");
